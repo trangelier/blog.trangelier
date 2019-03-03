e45f34a2b441d7b36a6fabb5199b6f16a903cdf4
Equinox32 <tylerangelier@gmail.com>
2019-03-02 21:14:50 -0500

    trangelier.dev blog

---
+++ src/components/Bio.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import styles from './Bio.module.css';
+import { getGravatarURL } from '../utils/getGravatarURL';
+
+function Bio(props) {
+	let photoURL = getGravatarURL({
+		email: 'tylerangelier@gmail.com',
+		size: 56
+	});
+
+	return (
+		<div
+			className={`
+      ${styles.Bio}
+      ${props.className || ''}
+    `}
+		>
+			<img src={photoURL} alt="Me" />
+			<p>
+				A simple blog that follows my life as a Full Stack Web
+				Developer. You can expect to see posts about{' '}
+				<a href="/tags/nodejs">NodeJS</a>, <a href="/tags/sql">SQL</a>,{' '}
+				<a href="/tags/plsql">PL/SQL</a>, some{' '}
+				<a href="/tags/systems">systems development</a>, and a little
+				<a href="/tags/react"> React</a>.
+			</p>
+		</div>
+	);
+}
+
+export default Bio;
---
+++ src/siteMetadata.js
@@ -0,0 +1,12 @@
+export default {
+	// The blog's title as it appears in the layout header, and in the document
+	// <title> tag.
+	title: 'blog.trangelier.dev',
+
+	author: 'Tyler R Angelier',
+	description:
+		'A simple blog as I progress through my Full-Stack Web Developer career.',
+
+	// The number of posts to a page on the site index.
+	indexPageSize: 15
+};
