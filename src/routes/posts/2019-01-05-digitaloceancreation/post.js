eba90f115d2674b5e8177b1a3f078685b275641f
Equinox32 <tylerangelier@gmail.com>
2019-03-03 00:10:05 -0500

    Deleted default post and created my first post

---
+++ src/routes/posts/2019-01-05-digitaloceancreation/post.js
@@ -0,0 +1,7 @@
+export default {
+	title: `DigitalOcean, Custom Domain, Reverse Proxy Nginx, and NodeJS!`,
+	tags: ['nodejs', 'systems'],
+	spoiler:
+		'Learn how to setup a Digital Ocean server, add a domain from Google Domain, configure a Reverse Proxy Nginx server, and setup NodeJS.',
+	getContent: () => import('./document.mdx')
+};
